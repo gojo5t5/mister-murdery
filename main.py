67d7999f08ffe7355fdee53b0d334bdc009e5ff9
gojo satoru <hematteo@outlook.com>
2022-03-05 22:01:00 +0000

    kmeans

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import victim_detect
 import process
 import sys
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
 def get_outline(filename: str)-> str:
     img = process.process_image(filename)
     return victim_detect.find_victim(filename, img)
 
-
+def k_means_processin(filepath: str) -> any:
+    pic = plt.imread(filepath)/255 
+    pic_n = pic.reshape(pic.shape[0]*pic.shape[1], pic.shape[2])
+    kmeans = KMeans(n_clusters=5, random_state=0).fit(pic_n)
+    pic2show = kmeans.cluster_centers_[kmeans.labels_]
+    cluster_pic = pic2show.reshape(pic.shape[0], pic.shape[1], pic.shape[2])
+    return cluster_pic
 
 if __name__ == "__main__":
-    get_outline(sys.argv[1])
+    get_outline(sys.argv[1]) 
